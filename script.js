d88c2294ddcffcc30bc1bc54d8b034189734bb24
iAlexx <80723925+iAlexx@users.noreply.github.com>
2024-08-18 09:36:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-const EVENTS_DELAY = 20000;
 const defaultLanguage = document.documentElement.getAttribute('lang')
 const gamePromoConfigs = {
+    MyCloneArmy: {
+        appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
+        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
+        eventsDelay: 70000,
+        attemptsNumber: 11
+    },
     ChainCube2048: {
         appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3'
+        promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
     TrainMiner: {
         appToken: '82647f43-3f87-402d-88dd-09a90025313f',
-        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954'
+        promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
     BikeRide3D: {
         appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-        promoId: '43e35910-c168-4634-ad4f-52fd764a843f'
-    },
-     MyCloneArmy: {
-        appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767'
+        promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
+        eventsDelay: 20000,
+        attemptsNumber: 22
     },
     MergeAway: {
         appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
         promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+        eventsDelay: 20000,
+        attemptsNumber: 10
     },
     TwerkRace: {
         appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-        promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c'
-    },
+        promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+        eventsDelay: 20000,
+        attemptsNumber: 10
+    }, 
 };
 
 let currentAppConfig = gamePromoConfigs.MyCloneArmy;
@@ -56,7 +67,7 @@ async function loadTranslations(language) {
         return await response.json();
     } catch (error) {
         console.error('Error loading translations:', error);
-        console.log('Failed to load translations. Check the console for details.');
+        alert('Failed to load translations. Check the console for details.');
         throw error;
     }
 }
@@ -153,15 +164,15 @@ document.getElementById('startBtn').addEventListener('click', async () => {
         try {
             clientToken = await login(clientId);
         } catch (error) {
-            console.log(`Failed to log in: ${error.message}`);
+            alert(`Failed to log in: ${error.message}`);
             startBtn.disabled = false;
             return null;
         }
-
-        for (let i = 0; i < 10; i++) {
-            await sleep(EVENTS_DELAY * delayRandom());
+        
+        for (let i = 0; i < currentAppConfig.attemptsNumber; i++) {
+            await sleep(currentAppConfig.eventsDelay * delayRandom());
             const hasCode = await emulateProgress(clientToken);
-            updateProgress(10 / keyCount);
+            updateProgress((100 / currentAppConfig.attemptsNumber) / keyCount);
             if (hasCode) {
                 break;
             }
@@ -171,7 +182,7 @@ document.getElementById('startBtn').addEventListener('click', async () => {
             const key = await generateKey(clientToken);
             return key;
         } catch (error) {
-            console.log(`Failed to generate key: ${error.message}`);
+            alert(`Failed to generate key: ${error.message}`);
             return null;
         }
     };
@@ -242,7 +253,7 @@ document.getElementById('startBtn').addEventListener('click', async () => {
 });
 
 document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-    window.open('https://t.me/iAlexMG', '_blank');
+    window.location.href = 'https://t.me/iAlexMG';
 });
 
 function generateClientId() {
