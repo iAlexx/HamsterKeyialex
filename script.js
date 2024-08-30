4b5a86001aec3f78b69ce21e2ccb0d1010d7c405
iAlexx <80723925+iAlexx@users.noreply.github.com>
2024-08-30 16:49:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const defaultLanguage = document.documentElement.getAttribute('lang')
 const gamePromoConfigs = {
-    MyCloneArmy: {
-        appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-        promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-        eventsDelay: 70000,
-        attemptsNumber: 11
-    },
     ChainCube2048: {
         appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
         promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
@@ -18,12 +12,6 @@ const gamePromoConfigs = {
         eventsDelay: 20000,
         attemptsNumber: 10
     },
-    BikeRide3D: {
-        appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-        promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-        eventsDelay: 20000,
-        attemptsNumber: 22
-    },
     MergeAway: {
         appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
         promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
@@ -35,16 +23,10 @@ const gamePromoConfigs = {
         promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
         eventsDelay: 20000,
         attemptsNumber: 10
-    }, 
+    },
     Polysphere: {
         appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-        eventsDelay: 20000,
-        attemptsNumber: 20
-    },
-    MudRacing: {
-        appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
-        promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
         eventsDelay: 20000,
         attemptsNumber: 20
     },
@@ -60,7 +42,7 @@ const gamePromoConfigs = {
         eventsDelay: 23000,
         attemptsNumber: 16
     },
-        GangsWars: {
+    GangsWars: {
         appToken: 'b6de60a0-e030-48bb-a551-548372493523',
         promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
         eventsDelay: 40000,
